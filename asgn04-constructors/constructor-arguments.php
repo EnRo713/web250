68eb9f387fea42f10bcfeed400d8e54dc5eaec10
EnRo713 <enriquearodriguez@students.abtech.edu>
2023-09-20 13:29:03 -0400

    Complete constructor exercise

--- asgn04-constructors/constructor-arguments.php
+++ asgn04-constructors/constructor-arguments.php
@@ -1,5 +1,20 @@
 <?php
 
-public function __construct($args[]) {
-  
+class Bird {
+  public $commonName;
+  public $latinName;
+
+  public function __construct($args=[]) {
+    $this->commonName = $args['commonName'] ?? NULL;
+    $this->latinName = $args['latinName'] ?? NULL;
+  }
 }
+
+$flycatcher = new Bird(['commonName'=>'Acadian Flycatcher', 'latinName'=>'Empidonax virescens']);
+echo "Common name: " . $flycatcher->commonName . "<br>";
+echo "Latin name: " . $flycatcher->latinName . "<br>";
+echo "<hr>";
+
+$wren = new Bird(['commonName'=>'Carolina Wren', 'latinName'=>'Thryothorus ludovicianus']);
+echo "Common name: " . $wren->commonName . "<br>";
+echo "Latin name: " . $wren->latinName . "<br>";
--- asgn04-constructors/constructor.php
+++ asgn04-constructors/constructor.php
@@ -1,9 +1,23 @@
 <?php
 
 class Bird {
+  public $commonName;
+  public $latinName;
 
-  // public commonName
+  public function __construct($commonName, $latinName) {
+    $this->commonName = $commonName;
+    $this->latinName = $latinName;
+  }
 
-  // public latinName 
-  
 }
+
+// Old way ↓
+// $robin->commonName = "American Robin";
+$robin = new Bird("American Robin", "Turdus migratorius");
+echo "Common name: " . $robin->commonName . "<br>";
+echo "Latin name: " . $robin->latinName . "<br>";
+echo "<hr>";
+
+$towhee = new Bird("Eastern Towhee", "Pipilo erythrophthalmus");
+echo "Common name: " . $towhee->commonName . "<br>";
+echo "Latin name: " . $towhee->latinName . "<br>";
